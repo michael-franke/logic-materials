e78a1fb1e97edd9af9372b669ef2f5a81b1c5750
michael-franke <the.secret.goldfish@me.com>
2022-10-19 14:41:22 +0200

    accept refactoring

--- 01-handouts/05-code-basic-prob.R
+++ 01-handouts/05-code-basic-prob.R
@@ -1,26 +1,26 @@
 library(tidyverse)
-theme_set(theme_bw() + theme(plot.background=element_blank()) )
+theme_set(theme_bw() + theme(plot.background = element_blank()) )
 
 # binomial distribution
-binom.plot.data = expand.grid(n = 24, theta = c(0.25, 0.5), k = 0:24) %>% 
+binom.plot.data <- expand.grid(n = 24, theta = c(0.25, 0.5), k = 0:24) %>% 
   mutate(probability = dbinom(k,n,theta), theta = as.factor(theta))
-binom.plot = ggplot(binom.plot.data, aes(x = k, y = probability, fill = theta)) + 
+binom.plot <- ggplot(binom.plot.data, aes(x = k, y = probability, fill = theta)) + 
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_manual(values = c("darkgrey", "firebrick")) + ylab("Binom(K = k; n=24, theta)")
 ggsave(binom.plot,filename = "00-pics/05_00_binomial-distribution.pdf", width = 6, height = 3)
 
 
 # cumulative distribution
-binom.plot.data = expand.grid(n = 24, theta = c(0.25, 0.5), k = 0:24) %>% 
+binom.plot.data <- expand.grid(n = 24, theta = c(0.25, 0.5), k = 0:24) %>% 
   mutate(probability = pbinom(k,n,theta), theta = as.factor(theta))
-binom.plot.cumulative = ggplot(binom.plot.data, aes(x = k, y = probability, fill = theta)) + 
+binom.plot.cumulative <- ggplot(binom.plot.data, aes(x = k, y = probability, fill = theta)) + 
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_manual(values = c("darkgrey", "firebrick")) + ylab("Binom(K <= k; n=24, theta)")
 ggsave(binom.plot.cumulative,filename = "00-pics/05_00_binomial-distribution-cumulative.pdf", width = 6, height = 3)
 
 
 # normal distribution
-normal.plot = ggplot(data.frame(x = c(-8, 8)), aes(x = x)) +
+normal.plot <- ggplot(data.frame(x = c(-8, 8)), aes(x = x)) +
   stat_function(fun = function(x) dnorm(x, sd = 4), aes(color = "4")) +
   stat_function(fun = function(x) dnorm(x, sd = 1), aes(color = "1")) +
   scale_colour_manual("standard deviation", breaks = c("4", "1"), values = c("firebrick", "darkgrey")) +
@@ -31,7 +31,7 @@ ggsave(normal.plot,
        width = 6, height = 3)
 
 # normal distribution cumulative
-normal.plot.cumulative = ggplot(data.frame(x = c(-8, 8)), aes(x = x)) +
+normal.plot.cumulative <- ggplot(data.frame(x = c(-8, 8)), aes(x = x)) +
   stat_function(fun = function(x) pnorm(x, sd = 4), aes(color = "4")) +
   stat_function(fun = function(x) pnorm(x, sd = 1), aes(color = "1")) +
   scale_colour_manual("standard deviation", breaks = c("4", "1"), values = c("firebrick", "darkgrey")) +
@@ -48,8 +48,8 @@ ggsave(normal.plot.cumulative,
 make_piechart <- function(value) {
   
   data <- data.frame(
-    group=factor(c('sunny', 'misty', 'rainy'), levels = c('sunny', 'misty', 'rainy')),
-    value=value
+    group = factor(c('sunny', 'misty', 'rainy'), levels = c('sunny', 'misty', 'rainy')),
+    value = value
   )
   
   # compute label position
@@ -59,13 +59,13 @@ make_piechart <- function(value) {
     mutate(ypos = cumsum(prop)- 0.5*prop )
   
   # Basic piechart
-  ggplot(data, aes(x="", y=prop, fill=group)) +
-    geom_bar(stat="identity", width=1, color="white") +
-    coord_polar("y", start=0) +
+  ggplot(data, aes(x = "", y = prop, fill = group)) +
+    geom_bar(stat = "identity", width = 1, color = "white") +
+    coord_polar("y", start = 0) +
     theme_void() + 
-    theme(legend.position="none") +
+    theme(legend.position = "none") +
     geom_text(aes(y = ypos, x = 1.2, label = group), color = "black", size=6.5) +
-    scale_fill_manual(values=c('cornsilk2', 'cornsilk3', 'cornsilk4'))
+    scale_fill_manual(values = c('cornsilk2', 'cornsilk3', 'cornsilk4'))
 }
 
 make_piechart(value = c(6,2,2))
